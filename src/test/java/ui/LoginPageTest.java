b9caf204c780d2ca0a9978af33adb5dd4456a17d
Alex Karamfilov <karamfilovs@gmail.com>
2024-03-11 21:51:07 +0200

    Adding headless execution to trigger the pipeline

--- src/test/java/ui/LoginPageTest.java
+++ src/test/java/ui/LoginPageTest.java
@@ -18,7 +18,7 @@ public class LoginPageTest {
     public void beforeEach() {
         //Do this before every test
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless=new"); //set headless execution
+        options.addArguments("--headless=new"); //set headless execution
         chrome = new ChromeDriver(options); // Start new chrome instance
         chrome.get(LOGIN_PAGE); //Navigate to login page (st2016.inv.bg)
     }
